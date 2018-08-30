1a6e3f177265e6229a926331a6328060426fc307
nicollas <nicollaslopesjf@gmail.com>
2018-08-30 20:52:06 -0300

    file

---
+++ aula-10-orientacao-a-objetos-classes-e-instancias/Program.cs
@@ -0,0 +1,31 @@
+﻿using Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula_10_orientacao_a_objetos_classes_e_instancias
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var clientes = Cliente.LerClientes();
+
+            /*var cliente = new Cliente();
+            cliente.Nome = "Nico";
+            cliente.Telefone = "555";
+            cliente.CPF = "666";
+            cliente.Gravar();
+
+            var cliente2 = new Cliente();
+            cliente2.Nome = "Lopes";
+            cliente2.Telefone = "777";
+            cliente2.CPF = "888";
+            cliente2.Gravar();*/
+
+         
+        }
+    }
+}
